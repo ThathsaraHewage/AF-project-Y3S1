046ddf534062c8893da1988d38d0dc2dd2238135
Thathsara <thathsara.hewage99@gmail.com>
2021-05-26 23:25:48 +0530

    landing page works finished

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,15 @@
 import { BrowserRouter as Router,Route} from "react-router-dom";
-import Navbar from './Components/Navbar';
-import Signup from './Components/Signup';
-import Header from './Components/Header';
-import Login from './Components/Login';
+//import Home from './Components/Home/Home';
+import React from 'react';
+import Home from './Components/solid/home';
+import Login from './Components/Auth/Login';
 
 function App() {
   return (
-    
     <Router>
       <div>
-        <Navbar/>
+        <Route path="/" exact component ={Home}/>
         <Route path="/login" exact component ={Login}/>
-        <Route path="/signup" exact component ={Signup}/>
-        <Route path="/home" exact component ={Header}/>
-        
       </div>  
     </Router>
      
